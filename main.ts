e08ec1740f37c262f557efa073746cd936606f80
l-freeze <mmmm.yamamoto@gmail.com>
2021-06-13 13:01:53 +0900

    settings.jsonのミス修正とimport_mapの読み込み修正

--- main.ts
+++ main.ts
@@ -1,2 +1,3 @@
-import { copy } from "https://deno.land/std@0.98.0/fs/copy.ts";
+//import { copy } from "https://deno.land/std@0.98.0/fs/copy.ts";
+import { copy } from "fs/copy.ts";
 copy("main.ts", "copy.ts");
